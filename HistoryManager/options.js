31545d10ca115434f136c0d32d07b4c99e690942
AbdulFattaah Popoola <abdulapopoola@outlook.com>
2023-01-10 09:53:43 -0800

    Aggregating by hostnames

--- HistoryManager/options.js
+++ HistoryManager/options.js
@@ -1,32 +1,7 @@
 const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
 const OneWeekAgo = (new Date).getTime() - millisecondsPerWeek;
 const maxResults = 100000;
-const Headers = [
-    "Rank",
-    "URL",
-    "Count"
-];
-
-chrome.history.search(
-    {
-        text: '',
-        startTime: OneWeekAgo,
-        maxResults: maxResults,
-    }, function (data) {
-        // data.forEach(function(page) {
-        //     console.log(page.url);
-        // });
-        console.log(data.length);
-        //let typed = data.find(a => a.typed);
-        //console.log(typed);
-        console.log(Object.keys(data[0]));
-
-        let table = document.querySelector("table");
-        let headerValues = Object.keys(data[0]);
-        generateTableHead(table, headerValues);
-        generateTableBody(table, data);
-    });
-
+const HEADERS = ['Hostname', 'Frequency', 'VisitCount'];
 
 function generateTableHead(table, headers) {
     let tHead = table.createTHead();
@@ -41,12 +16,69 @@ function generateTableHead(table, headers) {
 
 function generateTableBody(table, data) {
     let tBody = table.createTBody();
-    for (let entry of data) {
+    let keys = Object.keys(data);
+    for (let key of keys) {
         let row = tBody.insertRow();
+        let entry = data[key];
         for (let value of Object.values(entry)) {
+            if(Array.isArray(value)) {
+                continue; //todo; make this more elegant
+            }
             let text = document.createTextNode(value);
             let cell = row.insertCell();
             cell.appendChild(text);
         }
     }
 }
+
+/*
+    key: value
+    baseURL: [{
+        items:[historyItem]
+        count: count
+        visitCount: visitCount
+    }]
+
+    Table of objects
+    BaseURL, visitCount, 
+*/
+
+function transformHistoryItems(historyItems) {
+    let transformed = {};
+    for (let item of historyItems) {
+        let url = new URL(item.url);
+        let hostname = url.hostname;
+        let entry = transformed[hostname];
+        if (entry) {
+            entry.count++;
+            entry.visitCount += item.visitCount;
+            entry.items.push(item);
+        } else {
+            transformed[hostname] = {
+                hostname,
+                count: 1,
+                visitCount: item.visitCount,
+                items: [item]
+            }
+        }
+    }
+
+    return transformed;
+}
+
+function getHistory() {
+    chrome.history.search(
+        {
+            text: '',
+            startTime: OneWeekAgo,
+            maxResults: maxResults,
+        }, function (data) {
+            data = transformHistoryItems(data);
+            let table = document.querySelector("table");
+            // let headerValues = Object.keys(data[0]);
+            generateTableHead(table, HEADERS);
+            generateTableBody(table, data);
+        });
+}
+
+getHistory();
